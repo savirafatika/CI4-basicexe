cd1348a7814a7173d88a2d7471253a9ef25259d9
savirafatika <savira.fatika@students.amikom.ac.id>
2020-11-16 18:56:30 +0700

    example routing and controlling file

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,17 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
 		return view('welcome_message');
+	}
+
+	public function coba()
+	{
+		echo $this->nama;
 	}
 
 	//--------------------------------------------------------------------
